95d235c3fb37c2c6826c578d7562113f9817335b
Ryno Marree <ryno.marree@postnl.nl>
2022-08-14 17:13:50 +0200

    implement working delete, step towards idempotency

--- src/api.py
+++ src/api.py
@@ -36,15 +36,16 @@ class Api:
 
         self.get("/health")  # Test connection
         print('Successfully connected to the server.')
+        return self
 
-    def get(self, resource, id=None) -> dict:
+    def get(self, resource: str, id: int = None) -> dict:
         req = f"{self.base_url}{resource}{'/' + str(id) if id else ''}"
         print(f"Fetching: {req}")
         response = self.r.get(req)
         response.raise_for_status()
         return response.json()
 
-    def create(self, resource, body) -> None:
+    def create(self, resource: str, body: dict) -> None:
         req = f"{self.base_url}{resource}"
         try:
             response = self.r.post(req, json=body)
@@ -55,21 +56,22 @@ class Api:
             else:
                 raise HTTPError(f"{e.response.status_code} Error for resource: {resource}. Response: {e.response.text}")
 
-    def edit(self, resource, body, id) -> None:
+    def edit(self, resource: str, body: dict, id: int) -> None:
         req = f"{self.base_url}{resource}{'/' + str(id) if id else ''}"
-        current_settings = {}
         try:
             current_settings = self.get(resource, id)
         except HTTPError as e:
             if e.response.status_code == 404:
                 del body['id']  # can't create with existing ID
                 return self.create(resource, body)
+            else:
+                raise
         if not body.items() <= current_settings.items():  # only if updated / not default
             current_settings.update(body)  # apply changes
             response = self.r.put(req, json=current_settings)
             response.raise_for_status()
 
-    def delete(self, resource, id) -> None:
+    def delete(self, resource: str, id: int) -> None:
         req = f"{self.base_url}{resource}/{id}"
         print(f"Deleting: {req}")
         response = self.r.delete(req)
--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
-from api import Api
-from pyaml_env import parse_config
-from constants import Service, CONFIG_DEFAULT_LOCATION
 import yaml
+from pyaml_env import parse_config
+
+from api import Api
+from constants import Service, CONFIG_DEFAULT_LOCATION
 from routes import PATHS
 
 
@@ -12,19 +13,13 @@ class Config:
 
     @classmethod
     def from_yaml(cls, file: str = CONFIG_DEFAULT_LOCATION):
+        # TODO: add keys for unconfigured list items, such that they can get deleted
         cfg = parse_config(file, default_value='')
         return cls(cfg)
 
-    def apply(self):
-        for app in self._cfg:
-            self._api = Api(Service(app), **self._cfg[app].pop('server'))
-            self._api.initialize()
-            self._triage_and_apply(self._cfg[app])
-
     @classmethod
-    def from_current(cls, service, address, port):
-        api = Api(Service(service), address=address, port=port)
-        api.initialize()
+    def from_current(cls, service: str, address: str, port: int):
+        api = Api(Service(service), address=address, port=port).initialize()
         res = {}
         for path in PATHS[service]:
             value = api.get(path)
@@ -35,21 +30,32 @@ class Config:
         res['server'] = {'address': api.address, 'port': api.port}
         return cls({api.service: res})
 
+    def apply(self):
+        for app in self._cfg:
+            self._api = Api(Service(app), **self._cfg[app].pop('server')).initialize()
+            self._triage_and_apply(self._cfg[app])
+
     def to_file(self, filename):
         with open(filename, 'w') as file:
             yaml.dump(self._cfg, file)
 
-    def _triage_and_apply(self, object, resource: str = ''):
-        if isinstance(object, dict):
-            if any(isinstance(object[key], (dict, list)) for key in object):
-                for key in object:
-                    self._triage_and_apply(object[key], f"{resource}/{key}")
+    def _triage_and_apply(self, obj, resource: str = ''):
+        if isinstance(obj, dict):
+            if any(isinstance(obj[key], (dict, list)) for key in obj):
+                for key in obj:
+                    self._triage_and_apply(obj[key], f"{resource}/{key}")
             else:
-                print(f"calling edit: {resource=}")
-                self._api.edit(resource, object, object['id'])  # even non-list item has id (like config/ui)
-        elif isinstance(object, list):
-            for body in object:
-                if 'id' not in body:  # all list items should have id's
+                self._api.edit(resource, obj, obj['id'])  # even non-list item has id (like config/ui)
+        elif isinstance(obj, list):
+            for item in obj:
+                if 'id' not in item:  # all list items should have id's
                     raise
-                print(f"calling edit with id: {resource=}")
-                self._api.edit(resource, body, body['id'])
+                self._api.edit(resource, item, item['id'])
+            self._delete_excess_items(resource, obj)
+
+    def _delete_excess_items(self, resource, obj):
+        max_config_id = max((item['id'] for item in obj), default=0)
+        max_actual_id = max((item['id'] for item in self._api.get(resource)), default=-1)
+        if max_actual_id > max_config_id:
+            for id in range(max_config_id+1, max_actual_id+1):  # excluding start and including end
+                self._api.delete(resource, id)
