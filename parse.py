1faf853a0abdd8c522f1b3479a34f33d0ab4bc86
Ryno Marree <ryno.marree@postnl.nl>
2022-08-27 21:03:46 +0200

    add swagger files of all *arr services, initial add of routes

--- parse.py
+++ parse.py
@@ -1,16 +1,24 @@
 import json
-# from openapi_core import create_spec
 import re
 
+SWAGGER_FILES = ['docs/lidarr-v1.json', 'docs/prowlarr-v1.json', 'docs/radarr-v3.json', 'docs/readarr-v1.json',
+                 'docs/sonarr-v3.json']
 
-# spec = create_spec(data)
 
 def parse_swagger(file):
     with open(file) as f:
         data = json.load(f)
 
     paths = list({k: v for k, v in data['paths'].items() if 'get' in v}.keys())
-    filtered = [p for p in paths if not any(x in p for x in ['{', 'login', 'logout', 'routes', 'schema', 'initialize.js', 'health', 'calendar'])]
-    remove_prefix = [re.sub(r'/api/v[0-9]', '', p) for p in filtered]
-    return remove_prefix
+    # Some recurring irrelevant exclusions
+    filtered = [p for p in paths if not any(
+        x in p for x in
+        ['{', 'log', 'routes', 'schema', 'initialize.js', 'health', 'calendar', 'search', 'update', 'rename', 'wanted',
+         'history', 'queue', 'command', 'diskspace', 'parse', 'backup', 'blocklist'])]
+    removed_prefix = [re.sub(r'/api/v[0-9]', '', p) for p in filtered]
+    return removed_prefix
 
+
+d = {}
+for file in SWAGGER_FILES:
+    d[file[file.find('/') + 1:file.find('-')]] = parse_swagger(file)
--- src/run.py
+++ src/run.py
@@ -1,6 +1,13 @@
 from config import Config
+from constants import Service
 
-cfg = Config.from_yaml('test-cfg2.yml')
-cfg.apply()
 
-print('Finished applying configurations')
+if __name__ == "__main__":
+    cfg = Config.from_current('radarr', 'http://192.168.178.47', 7878)
+    cfg.to_file('radarr-test.yml')
+
+
+    # cfg = Config.from_yaml('test-cfg2.yml')
+    # cfg.apply()
+    #
+    # print('Finished applying configurations')
