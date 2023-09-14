a0c6efc0aa4d8ee35e5df552a39291c0b3e3eca8
Aidan Hilt <aidanhilt2@gmail.com>
2023-09-13 22:12:54 -0400

    Added env var config for config path, and logic to try getting init info for both v3 and v4 application

--- src/run.py
+++ src/run.py
@@ -1,19 +1,25 @@
 import yaml
+import os
 from api import Api
 
-configs = yaml.safe_load(open('./config/config.yml', 'r'))
+config_path = "./config/config.yml"
+
+if "FLEMMARR__CONFIG_PATH" in os.environ:
+    config_path = os.environ["FLEMMARR__CONFIG_PATH"]
+
+configs = yaml.safe_load(open(config_path, "r"))
 
 for key in configs:
     config = configs[key]
-    server = config['server']
-    config.pop('server')
+    server = config["server"]
+    config.pop("server")
 
-    api = Api(server['address'], server['port'])
+    api = Api(server["address"], server["port"])
 
-    print('Trying to connect to {}'.format(key))
+    print("Trying to connect to {}".format(key))
     api.initialize()
 
-    print('Starting to apply configuration')
+    print("Starting to apply configuration")
     api.apply(config)
 
-print('Finished to apply configurations')
+print("Finished to apply configurations")
